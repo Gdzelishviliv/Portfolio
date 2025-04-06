355512d04308137afcd8d93c5b70d54c516582ab
Nanno <gdzelishviliv@gmail.com>
2025-04-06 16:19:33 +0400

    change typewriter library

--- src/app/components/atoms/Typewriter.tsx
+++ src/app/components/atoms/Typewriter.tsx
@@ -1,23 +1,22 @@
-import React from "react";
-import TypewritingEffect from "typewriter-effect";
+import { useTypewriter } from "react-simple-typewriter";
 
 const Typewriter = () => {
+  const [text] = useTypewriter({
+    words: [
+      "Full Stack Web Developer",
+      "Next.js Enthusiast",
+      "Software Engineer",
+      "Tech Problem Solver",
+      "Lifelong Learner",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    deleteSpeed: 50,
+    delaySpeed: 1000,
+  });
   return (
     <h2 className="text-white">
-      <TypewritingEffect
-        options={{
-          strings: [
-            "Full Stack Web Developer",
-            "Next.js Enthusiast",
-            "Software Engineer",
-            "Tech Problem Solver",
-            "Lifelong Learner",
-          ],
-          autoStart: true,
-          loop: true,
-          delay: 100,
-        }}
-      />
+      {text}<span className="cursor-blink">|</span>
     </h2>
   );
 };
