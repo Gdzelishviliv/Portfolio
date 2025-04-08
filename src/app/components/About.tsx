8fb1915adc69d5b06e3d9e68d063870aa656354b
Nanno <gdzelishviliv@gmail.com>
2025-04-08 14:29:05 +0400

    changed image size for mobile and fixed spaces

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,30 +6,30 @@ import Typewriter from "./atoms/Typewriter";
 const About = () => {
   return (
     <>
-      <div className="mx-[16px] mt-[20px] relative">
+      <div className="mx-[16px] mt-[25px] relative">
         <div className="flex flex-col">
           <h1 className="text-white text-[24px]">
             Hi I<span className="gradient-text">’</span>m
           </h1>
-          <h2 className="gradient-text text-[23px] mt-[20px]">
+          <h2 className="gradient-text text-[23px] mt-[5px]">
             Ivane
             <br /> Gdzelishvili
           </h2>
           <Typewriter/>
-          <p className="text-white text-[14px] mt-[20px] min-w-[200px] max-w-[220px]">
+          <p className="text-white text-[14px] mt-[15px] min-w-[200px] max-w-[220px]">
             a passionate Full Stack Web Developer with 2 years of experience in
             building dynamic and scalable web applications. I specialize in both
             front-end and back-end development, working with modern technologies
             to create seamless, user-friendly, and performance-driven solutions.
           </p>
         </div>
-        <div className="w-fit absolute right-0 top-[-25px] z-[-1]">
+        <div className="w-fit absolute right-0 top-[-30px] z-[-1]">
           <Image
-            className="rounded-4xl"
+            className="rounded-4xl "
             src={ProfileImg}
             alt="Profile Image"
-            width={200}
-            height={200}
+            width={120}
+            height={100}
           />
         </div>
       </div>
